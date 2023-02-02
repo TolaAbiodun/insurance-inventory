5b2f478e22c992e0eed5051a7c463ae3aaa41ca1
TolaAbiodun <adetolaabiodun@outlook.com>
2023-02-02 11:50:25 +0100

    updates deploy config, fix dark mode issues on android >10

--- src/utils/data.js
+++ src/utils/data.js
@@ -15,20 +15,22 @@ export const inventoryData = [
     purchasePrice: 999.99,
     category: 'Art',
     imgUrl:
-      'https://i.ibb.co/mcjdVMj/13051478-6-femme-qui-va-de-lavant-carton-entoile33x24.jpg',
+      'https://www.vintagemakeup.fr/wp-content/uploads/2022/09/calendrier-blissim.webp',
   },
   {
     id: uuidv4(),
     name: 'Cartier ring',
     purchasePrice: 199.99,
     category: 'Jewelry',
-    imgUrl: 'https://i.ibb.co/yWmLXMq/cartier-ring.jpg',
+    imgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9sy_7bXgJmkwf9VIM_IUSZQirYqjxgRZWPA&usqp=CAU',
   },
   {
     id: uuidv4(),
     name: 'Guitar',
     purchasePrice: 299.99,
     category: 'Musical Instrument',
-    imgUrl: 'https://i.ibb.co/4dfndL2/louis-hansel-M-d-J-Scwa-LE-unsplash.jpg',
+    imgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS6p_UbTQm08HDjGAAB74bO54-K5FBgN3_uw&usqp=CAU',
   },
 ];
